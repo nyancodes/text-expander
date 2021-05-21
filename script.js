442ce7abca386e8629182c43f2232577eb89c9fd
Nyan Htet <nhtet@internal.salesforce.com>
2021-05-21 16:36:34 -0700

    added function to replace text

---
+++ script.js
@@ -0,0 +1,26 @@
+let shortcuts = {}
+
+
+$('form').on("submit", function (e) {
+    e.preventDefault();
+    shortcuts[$("#shortcut").val()] = $("#stext").val();
+    console.log(shortcuts);
+
+});
+
+
+
+
+$('.test').on('keyup', function() {
+     if (this.value === ".sig") {
+             return this.value = this.value.replace('.sig', 'Nyan Htet \nTechforce Analyst | Portland, OR' );
+             };
+        
+});
+
+
+
+
+
+
+

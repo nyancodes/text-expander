d834cf3096df74a4b0a1ddb6fe5308a82412571c
Nyan Htet <nhtet@internal.salesforce.com>
2021-05-28 14:13:08 -0700

    created base files

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,9 @@
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.runtime.openOptionsPage();
+  })
+
 chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab);
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        console.log(current_tab_info.url);
+    });
 });

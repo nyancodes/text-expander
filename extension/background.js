d55bf9c43bbb32dce45786c49e4dbdae683d976f
Nyan Htet <nyanhtet@yahoo.com>
2021-06-04 12:19:38 -0700

    Update background.js
    added promise

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(() => {
     chrome.runtime.openOptionsPage();
-  })
+})
 
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
@@ -8,71 +8,218 @@ chrome.tabs.onActivated.addListener(tab => {
     });
 });
 
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'insert') {
+        let request = insert_records(request.payload);
+
+        request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "insert_success",
+                payload: res
+            })
+        })
+    }
+    else if (request.message === 'get') {
+        let request = get_records(request.payload);
+
+        request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "get_success",
+                payload: res
+            })
+        })
+    }
+    else if (request.message === 'update') {
+        let request = update_record(request.payload);
+
+        request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "update_success",
+                payload: res
+            })
+        })
+    }
+    else if (request.message === 'delete') {
+        let request = delete_records(request.payload);
+
+        request.then(res => {
+            chrome.runtime.sendMessage({
+                message: "delete_success",
+                payload: res
+            })
+        })
+    }
+
+});
+
+/* --------------------------------------------------------- */
+/* DATABASE stuff */
 
 let db = null;
+let shorts = [
+    {
+        ".thx": "Thank you for contacting Techforce!"
+    },
 
+    {
+        ".sig": "Nyan Htet \nTechforce Analyst | Portland, OR"
+    }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+]
 
+function create_db() {
 
-function create_db(){
-    
     const request = window.indexedDB.open('Database');
-    
-    request.onerror = function(event){
+
+    request.onerror = function (event) {
         console.log("Problem opening DB");
     }
 
-    request.onupgradeneeded = function(event){
-        db = event.target.result; 
-        let objectStore = db.createObjectStore('roster', {
+    request.onupgradeneeded = function (event) {
+        db = event.target.result;
+        let objectStore = db.createObjectStore('shorts', {
             keyPath: 'shortcut'
         })
-        objectStore.transaction.oncomplete = function(event) {
+        objectStore.transaction.oncomplete = function (event) {
             console.log("Shortcut created");
         }
 
     }
 
-    request.onsuccess = function(event){
-        db = event.target.result; 
+    request.onsuccess = function (event) {
+        db = event.target.result;
         console.log("DB Opened");
     }
 
 }
 
-function delete_db(){
-    
+function delete_db() {
+
     const request = window.indexedDB.delete('Database');
-    
-    request.onerror = function(event){
+
+    request.onerror = function (event) {
         console.log("Problem opening DB");
     }
 
-    request.onsuccess = function(event){
-        db = event.target.result; 
+    request.onsuccess = function (event) {
+        db = event.target.result;
         console.log("DB Deleted");
     }
 
 }
 
-function insert_records(records){
-    if(db){
-        const insert_transaction = db.transcation("roster", "readwrite");
+function insert_records(records) {
+    if (db) {
+
+
+        const insert_transaction = db.transcation("shorts", "readwrite");
         const objectStore = insert_transaction.objectStore("roster");
-        objectStore.add({
-            
+
+        return new Promise((resolve, reject) => {
+
+            insert_transaction.oncomplete = function () {
+                console.log("inserted");
+                resolve(true);
+            }
+
+            insert_transaction.onerror = function () {
+                console.log("Error inserting records");
+                resolve(false);
+            }
+
+
+            shorts.forEach(s => {
+                let request = objectStore.add(s);
+
+                request.onsuccess = function () {
+                    console.log("Added", s);
+                }
+
+            });
+
+
         })
+
     }
 }
 
 
+function get_record(record) {
+
+    if (db) {
+
+        const get_transaction = db.transcation("shorts", "readwrite");
+        const objectStore = insert_transaction.objectStore("roster");
 
 
+        return new Promise((resolved, reject) => {
+
+            get_transaction.oncomplete = function () {
+                console.log("Get transaction completed");
+            }
+
+            get_transaction.onerror = function () {
+                console.log("Error getting records");
+            }
+
+            let request = objectStore.get(shortcut);
+
+            request.onsuccess = function (event) {
+                resolve(event.target.result);
+            }
+        })
+    }
+}
+    function update_record(record) {
+
+        if (db) {
+
+            const put_transaction = db.transcation("shorts", "readwrite");
+            const objectStore = insert_transaction.objectStore("shorts");
+
+            return new Promise((resolve, reject => {
+
+                put_transaction.oncomplete = function () {
+                    console.log("all put transaction completed");
+                    resolve(true);
+                }
+
+                put_transaction.onerror = function () {
+                    console.log("Error updating records");
+                    resolve(false);
+                }
+
+                objectStore.put(record);
+
+            }));
+
+        }
+
+    }
 
 
+    function delete_record(shortcut) {
+
+        if (db) {
+
+            const put_transaction = db.transcation("shorts", "readwrite");
+            const objectStore = insert_transaction.objectStore("shorts");
 
 
+            return new Promise((resolve, reject) => {
 
+                delete_transaction.oncomplete = function () {
+                    console.log("all delete transaction completed");
+                    resolve(true);
+                }
 
-});
+                delete_transaction.onerror = function () {
+                    console.log("Error deleting records");
+                    resolve(flase);
+                }
+
+                objectStore.delete(email);
+            })
+        }
+    }
+
